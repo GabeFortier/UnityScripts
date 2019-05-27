8d3da51b1fdf4a4ccb95f5d0e13b0dfe777d3a37
GabeFortier <gfortier5130@hotmail.com>
2019-05-27 01:54:31 -0700

    Added two base scripts
    This starts the repository with an ammo switching script, and a simple 2D player controller(jump is still a work in progress).

---
+++ PLayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private float distanceToGround;
+    private Collider2D colide;
+    private Collider2D ground;
+    private int ammoSelect;
+    private GameObject ammo;
+
+    public float speed;
+
+    void Start(){
+        rb2d = GetComponent<Rigidbody2D> ();
+        ground = GameObject.FindGameObjectWithTag("Ground").GetComponent<Collider2D>();
+        colide = GetComponent<Collider2D>();
+        distanceToGround = colide.bounds.extents.y;
+    }
+    bool isGrounded(){
+        
+        return colide.IsTouching(ground);
+
+    }
+   
+    void  Update()
+    {
+        if(Input.GetKey(KeyCode.Alpha1)){
+            
+        }
+    }
+    void FixedUpdate(){
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+        Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+  
+        if (Input.GetKey(KeyCode.Space) && isGrounded())
+        {
+            rb2d.velocity = Vector2.zero;
+            rb2d.AddForce(new Vector2(0,10), ForceMode2D.Impulse);
+            
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            rb2d.velocity = Vector2.zero;
+            rb2d.AddForce(Vector3.down,ForceMode2D.Impulse);
+        }
+    }
+}
+
+
---
+++ selectAmmo.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class selectAmmo : MonoBehaviour
+{
+    public int selectedAmmo = 0;
+    void Start()
+    {
+        ammoPick();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        int lastAmmo = selectedAmmo;
+
+        if(Input.GetAxis("Mouse ScrollWheel") > 0f){
+            if(selectedAmmo >= transform.childCount -1){
+                selectedAmmo = 0;
+            }
+            else{
+                selectedAmmo++;
+            }
+            
+        }
+        if(Input.GetAxis("Mouse ScrollWheel") < 0f){
+            if(selectedAmmo <= 0){
+                selectedAmmo = transform.childCount - 1;
+            }
+            else{
+                selectedAmmo--;
+            }
+        }
+        if(lastAmmo != selectedAmmo){
+            ammoPick();
+        }
+    }
+    void ammoPick(){
+        int i = 0;
+        foreach(Transform ammo in transform){
+        if(i== selectedAmmo){
+            ammo.gameObject.SetActive(true);
+        }
+        else{
+            ammo.gameObject.SetActive(false);
+        }
+        i++;
+        }
+    }
+}
+
