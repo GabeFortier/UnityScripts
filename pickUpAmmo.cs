6b12ae1fc480d55f64b878d4f39d9bba8f68e908
GabeFortier <gfortier5130@hotmail.com>
2019-06-08 23:28:43 -0700

    Pick up an ammo.
    This script picks up an ammo item and adds it to the player's ammo amount dictionary.

---
+++ pickUpAmmo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickUpAmmo : MonoBehaviour
+{
+      private  GameObject thePlayer;
+       private IDictionary<string, int> ammos;
+        void Start()
+    {
+        
+       
+    }     
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("entered");
+        thePlayer= GameObject.FindGameObjectWithTag("Player");
+        if(other.gameObject.CompareTag("ammo")){
+            other.gameObject.SetActive(false);
+            ammos = thePlayer.GetComponent<PLayerController> ().ammoCounts;
+            ammos[other.gameObject.name] = ammos[other.gameObject.name] + 1;
+            Debug.Log(ammos[other.gameObject.name]);
+        
+        
+        }
+    }
+
+}
